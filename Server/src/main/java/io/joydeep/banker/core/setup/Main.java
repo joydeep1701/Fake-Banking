c6aedb324f8bf6bd771090aad2114e2a0b2dc076
Joydeep Mukherjee <joydeep1701@gmail.com>
2018-10-24 02:51:16 -0100

    Removed logger from main setup

--- Server/src/main/java/io/joydeep/banker/core/setup/Main.java
+++ Server/src/main/java/io/joydeep/banker/core/setup/Main.java
@@ -1,14 +1,10 @@
 package io.joydeep.banker.core.setup;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.apache.log4j.BasicConfigurator;
 
 public class Main {
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.info("Hello World");
-//        Permissions.createPermissionsTable();
+        Permissions.createPermissionsTable();
     }
 }
